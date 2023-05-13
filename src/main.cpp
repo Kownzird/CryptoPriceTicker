fca5ebb72bce41c1da363aacfd5777d43f3b6b8c
Kownzird <1597411647@qq.com>
2023-05-13 20:30:58 +0800

    导入LVGL库新增币种logo & 优化币种切换功能

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,14 @@
-
-/* 
-code	color
-0x0000	Black
-0xFFFF	White
-0xBDF7	Light Gray
-0x7BEF	Dark Gray
-0xF800	Red
-0xFFE0	Yellow
-0xFBE0	Orange
-0x79E0	Brown
-0x7E0	Green
-0x7FF	Cyan
-0x1F	Blue
-0xF81F	Pink
-
- */
-
 #include <TFT_eSPI.h> // Graphics and font library for ST7735 driver chip
 #include <SPI.h>
+#include <lvgl.h>
+#include <misc/lv_style.h>
+// #include <font/lv_font.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include "../lib/ArduinoJson/ArduinoJson.h"
 
 #define VERSION "v1.0.02"
+#define LV_USE_LOG 0
 
 #define KEY35 35
 
@@ -42,6 +28,15 @@ String wifiMsgBuf[10][2] = {
 	{"xxx3","yyy3"}
 };
 
+static const uint16_t screenWidth  = TFT_HEIGHT;
+static const uint16_t screenHeight = TFT_WIDTH;
+
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t buf[ screenHeight*screenWidth / 10 ];
+
+lv_obj_t *logoImg = NULL;
+lv_obj_t *label = NULL;
+static lv_style_t style;
 
 DynamicJsonDocument doc(2048);
 
@@ -50,8 +45,32 @@ double coinPrice = -1; //WBTC价格
 String coinPriceStr = "";
 String chainShortName = "eth";
 int currentCoinDisplayMode = BTC_MODE;
+int lastCoinDisplayMode = BTC_MODE;
 
 TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
+
+#if LV_USE_LOG != 0
+/* Serial debugging */
+void my_print(const char * buf)
+{
+    Serial.printf(buf);
+    Serial.flush();
+}
+#endif
+
+/* Display flushing */
+void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p )
+{
+    uint32_t w = ( area->x2 - area->x1 + 1 );
+    uint32_t h = ( area->y2 - area->y1 + 1 );
+
+    tft.startWrite();
+    tft.setAddrWindow( area->x1, area->y1, w, h );
+    tft.pushColors( ( uint16_t * )&color_p->full, w * h, true );
+    tft.endWrite();
+
+    lv_disp_flush_ready( disp );
+}
 
 //获取已存储wifi信息数量
 int getWifiMsgCount(){
@@ -115,32 +134,60 @@ void WiFi_Connect()
 
 		//未超时，已正常连接
 		if(connectFlag){
+			tft.fillScreen(TFT_BLACK);
 			return;
 		}
 		
 	}
 }
 
-
 //KEY35中断处理函数
 void changeCoinDisplayMode(){
+	int count = 1000;
 
 	if(digitalRead(KEY35) == LOW){
+		
+		//消抖
+		while(count--){};
 
-		//切换显示模式标志位
-		Serial.println("change Display Mode");
+		if(digitalRead(KEY35) == LOW){
 
-		tft.fillScreen(TFT_BLACK);
-		tft.setTextColor(TFT_WHITE, TFT_BLACK);
+			//切换显示模式标志位
+			Serial.println("change Display Mode");
 
-		if(currentCoinDisplayMode == BTC_MODE){
-			currentCoinDisplayMode = ETH_MODE;
-			tft.drawCentreString("ETH  MODE",120,60,4);
-		}else{
-			currentCoinDisplayMode = BTC_MODE;
-			tft.drawCentreString("BTC  MODE",120,60,4);
+			// tft.fillScreen(TFT_BLACK);
+			// tft.setTextColor(TFT_WHITE, TFT_BLACK);
+
+			if(currentCoinDisplayMode == BTC_MODE){
+				currentCoinDisplayMode = ETH_MODE;
+				// tft.drawCentreString("ETH  MODE",120,60,4);
+
+				//显示BTC LOGO
+				LV_IMG_DECLARE(ETH_Logo_50x50);
+				lv_img_set_src(logoImg, &ETH_Logo_50x50);
+				lv_obj_set_pos(logoImg, 15, 42);
+
+				lv_style_set_text_font(&style, &lv_font_montserrat_20);
+				lv_style_set_text_color(&style, lv_palette_main(LV_PALETTE_GREY));
+				lv_obj_add_style(label, &style, 0);
+				lv_obj_align( label, LV_ALIGN_LEFT_MID, 90, 0 );
+				lv_label_set_text( label, "ETH  MODE");
+				lv_timer_handler();
+			}else{
+				currentCoinDisplayMode = BTC_MODE;
+				//显示BTC LOGO
+				LV_IMG_DECLARE(BTC_Logo_50x50);
+				lv_img_set_src(logoImg, &BTC_Logo_50x50);
+				lv_obj_set_pos(logoImg, 15, 42);
+
+				lv_style_set_text_font(&style, &lv_font_montserrat_20);
+				lv_style_set_text_color(&style, lv_palette_main(LV_PALETTE_ORANGE));
+				lv_obj_add_style(label, &style, 0);
+				lv_obj_align( label, LV_ALIGN_LEFT_MID, 90, 0 );
+				lv_label_set_text( label, "BTC  MODE");
+				lv_timer_handler();
+			}
 		}
-
 	}
 }
 
@@ -158,12 +205,41 @@ void setup(void) {
 	tft.init();
 	tft.setRotation(1);
 	tft.fillScreen(TFT_BLACK);
-	tft.setTextColor(TFT_YELLOW, TFT_BLACK); // Note: the new fonts do not draw the background colour
+	tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+
+	lv_init();    // 初始化 LVGL 库
+#if LV_USE_LOG != 0
+    lv_log_register_print_cb( my_print ); /* register print function for debugging */
+#endif
+
+	lv_disp_draw_buf_init( &draw_buf, buf, NULL,screenHeight*screenWidth / 20 );
+
+	// 初始化缓冲区
+    static lv_disp_drv_t disp_drv;
+    lv_disp_drv_init( &disp_drv );
+
+    disp_drv.hor_res = screenWidth;
+    disp_drv.ver_res = screenHeight;
+    disp_drv.flush_cb = my_disp_flush;   // 设置刷新回调函数
+    disp_drv.draw_buf = &draw_buf;   // 设置缓冲区
+    lv_disp_drv_register( &disp_drv ); // 注册驱动程序
+
+	lv_color_t black = lv_color_make(0x00, 0x00, 0x00); // 设置颜色常量
+    lv_obj_t * scr = lv_disp_get_scr_act(NULL); // 获取当前屏幕对象
+	lv_obj_set_style_bg_color(scr, black, 0); // 设置屏幕背景色
+
+	//LOGO对象
+	logoImg = lv_img_create(lv_scr_act());
+
+	//显示获取价格标签对象
+    label = lv_label_create(lv_scr_act());
+
+	//初始化样式
+    lv_style_init(&style);
+
 
 	Serial.print("Connecting..");
-
 	WiFi_Connect();
-
 	Serial.println("\r\nWiFi connected");
 
 	Serial.println("IP address: ");
@@ -261,14 +337,36 @@ double getETHPrices(){
 }
 
 void loop() {
-	char buf[10];
-	int fontType = 6;
-	bool reconnectFlag = false;
+start:
+
+	static char buf[10];
+	static bool reconnectFlag = false;
+
+	//设置字体大小
+	lv_style_set_text_font(&style, &lv_font_montserrat_30);
 
 	if(currentCoinDisplayMode == BTC_MODE){
 		coinPrice = getBitcoinPrices();
+
+		//显示BTC LOGO
+		LV_IMG_DECLARE(BTC_Logo_50x50);
+		lv_img_set_src(logoImg, &BTC_Logo_50x50);
+		lv_obj_set_pos(logoImg, 15, 42);
+
+		//设置BTC价格颜色
+		lv_style_set_text_color(&style, lv_palette_main(LV_PALETTE_ORANGE));
+
 	}else if(currentCoinDisplayMode == ETH_MODE){
 		coinPrice = getETHPrices();
+
+		//显示ETH LOGO
+		LV_IMG_DECLARE(ETH_Logo_50x50);
+		lv_img_set_src(logoImg, &ETH_Logo_50x50);
+		lv_obj_set_pos(logoImg, 15, 42);
+
+		//设置ETH价格颜色
+		lv_style_set_text_color(&style, lv_palette_main(LV_PALETTE_GREY));
+		
 	}
 	
 	if(coinPrice < 0){
@@ -277,33 +375,38 @@ void loop() {
 
 			//重连后，重新计数出错次数
 			getPriceErrCount = 0;
-			
 		}
-
-		fontType = 4;
-		tft.fillScreen(TFT_BLACK);
-		tft.setTextColor(TFT_RED, TFT_BLACK);
 		sprintf(buf, "ERROR");
+
+		//设置出错颜色
+		lv_style_set_text_color(&style, lv_palette_main(LV_PALETTE_RED));
 	}else{
-		fontType = 6;
-		tft.fillScreen(TFT_BLACK);
-		tft.setTextColor(TFT_ORANGE, TFT_BLACK);
-		sprintf(buf, "%.2lf", coinPrice);
+		sprintf(buf, "$%.0lf", coinPrice);
+		
 	}
 
 	coinPriceStr = String(buf);
 	Serial.println(coinPriceStr);
-	
-	// tft.drawString(coinPriceStr, 20, TFT_WIDTH/2-20, 6);
-	tft.drawCentreString(coinPriceStr.c_str(),120,50, fontType);
 
+	lv_obj_add_style(label, &style, 0);
+    lv_obj_align( label, LV_ALIGN_LEFT_MID, 90, 0 );
+	lv_label_set_text( label, coinPriceStr.c_str() );
+
+	//重新连接Wifi
 	if(reconnectFlag){
-		//重新连接Wifi
 		WiFi_Connect();
+		goto start;
 	}
 
-	delay(1000 * 10);
-}
+	lv_timer_handler();
 
+	for(int i=0; i<10; i++){
+		delay(1000);
+		if(lastCoinDisplayMode != currentCoinDisplayMode){
+			lastCoinDisplayMode = currentCoinDisplayMode;
+			break;
+		}
+	}
+}
 
 
